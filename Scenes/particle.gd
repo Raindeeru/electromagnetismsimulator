85d369badec5cd974f38a1c5a73b366e6d716b2f
Raindeeru <derv.gonzales17@gmail.com>
2024-12-14 16:47:05 +0800

    added sprite

--- Scenes/particle.gd
+++ Scenes/particle.gd
@@ -2,11 +2,24 @@ extends RigidBody2D
 
 #in microcoloumbs
 @export var charge:float = 1 
+@onready var base = $Base
+@onready var positive = $Positive
+@onready var negative = $Negative
 const microcoloumb = 0.000001
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if (charge > 0):
+		positive.visible = true
+		negative.visible = false
+		base.self_modulate = Color.RED
+	if (charge < 0):
+		negative.visible = true
+		positive.visible = false
+		base.self_modulate = Color.BLUE
+		
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
