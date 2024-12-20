063bd53097252c4ca8daaf5a32a460c4e722ed9b
Raindeeru <derv.gonzales17@gmail.com>
2024-12-20 20:30:13 +0800

    added coloring and a little bit of the ui

--- Scenes/ElectricField.gd
+++ Scenes/ElectricField.gd
@@ -3,15 +3,29 @@ class_name  ElectricField
 
 const coloumb_constant:float = 8987551786.2
 @onready var particles = get_children()
+signal added_signal
+
+var particle_ref = preload("res://Scenes/particle.tscn")
+
+var adding:bool
+var mouse_on_field: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if adding and mouse_on_field and event.is_action_pressed("left_mouse"):
+		var particle = particle_ref.instantiate()
+		particle.position = get_global_mouse_position()
+		add_child(particle)
+		particles = get_children()
+		adding = false
+		added_signal.emit()
 
 func coloumbs_law(charge1:float, charge2:float, radius: float):
 	return (charge1 * charge2 * coloumb_constant)/pow(radius, 2)
@@ -25,10 +39,23 @@ func get_electric_field(pos:Vector2):
 		var r = pos.distance_to(particle.position)
 		
 		var a = particle.position.angle_to_point(pos)
-		print(a)
-		print(pos)
 		var field = coloumbs_law(particle.charge, 1, r)
 		
 		net_field.x += cos(a) * field
 		net_field.y += sin(a) * field
 	return net_field
+
+func _on_add_pressed():
+	if adding:
+		adding = false
+	else:
+		adding = true
+
+
+
+func _on_control_mouse_entered():
+	mouse_on_field = true
+
+
+func _on_control_mouse_exited():
+	mouse_on_field = false
--- Scenes/Renderer.gd
+++ Scenes/Renderer.gd
@@ -5,19 +5,31 @@ var x_points_size: int
 var y_points_size: int
 var points_resolution = 50
 
+var gradient_data := {
+	0.0: Color.BLACK,
+	0.20: Color.WEB_PURPLE,
+	0.5: Color.RED,
+	0.70: Color.YELLOW,
+	1.0: Color.WHITE
+}
+@onready var gradient := Gradient.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	gradient.offsets = gradient_data.keys()
+	gradient.colors = gradient_data.values()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _draw():
+	print("drawing vector field")
 	var view_size = get_viewport().get_visible_rect().size
-	print(view_size)
 	x_points_size = view_size.x/points_resolution + 1
 	y_points_size = view_size.y/points_resolution + 1
+	
+	var max = 0
 	
 	for i in range(x_points_size):
 		for j in range(y_points_size):
@@ -25,11 +37,11 @@ func _draw():
 			add_child(field_label)
 			field_label.set_position(Vector2(i*points_resolution, j*points_resolution))
 			
+			
 			var field_vec = electric_field.get_electric_field(field_label.position)
 			var field_mag = snappedf(sqrt(pow(field_vec.x, 2) + pow(field_vec.y, 2)), 0.01)
-			var field_mag_reduced = min(50, field_mag/5000)
+			var field_mag_reduced = min(20, field_mag/5000)
 			var field_vec_redcuced = field_vec.normalized() * field_mag_reduced
-			
 			#arrow ends
 			var angle1 = field_vec_redcuced.angle() + PI/2 + PI/4
 			var angle2 = field_vec_redcuced.angle() - PI/2 - PI/4
@@ -40,11 +52,27 @@ func _draw():
 			
 			var arrow_end_2 = field_vec_redcuced + normal_angle_vector2 * field_mag_reduced/5
 			
+			var scaled_point = log(field_mag)/log(2)
 			
-			draw_line(field_label.position, field_label.position + field_vec_redcuced, Color.ALICE_BLUE, 1, true )
-			draw_line(field_label.position + field_vec_redcuced, field_label.position + arrow_end_1, Color.ALICE_BLUE, 1, true)
-			draw_line(field_label.position + field_vec_redcuced, field_label.position + arrow_end_2, Color.ALICE_BLUE, 1 , true)
+			var mapped_point = remap(scaled_point, 13, 25, 0, 1)
 			
+			var arrow_color = gradient.sample(mapped_point)
+			
+			draw_line(field_label.position, field_label.position + field_vec_redcuced, arrow_color, 1, true )
+			draw_line(field_label.position + field_vec_redcuced, field_label.position + arrow_end_1, arrow_color, 1, true)
+			draw_line(field_label.position + field_vec_redcuced, field_label.position + arrow_end_2, arrow_color, 1 , true)
+			
+			print(scaled_point)
+			
+			if scaled_point > max:
+				max = scaled_point
+		
+	print(max)
 
 func vector_magnitude(vec:Vector2):
 	return sqrt(pow(vec.x, 2) + pow(vec.y, 2))
+
+
+func _on_electric_field_added_signal():
+	queue_redraw()
+
